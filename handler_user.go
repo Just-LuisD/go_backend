a09b55589334da3089cb194120c3b3f96ee58af8
Just-LuisD <luis.e.duran.engr@gmail.com>
2024-06-09 11:49:27 -0500

    finished tutorial

--- handler_user.go
+++ handler_user.go
@@ -40,3 +40,17 @@ func (apiConfig *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Requ
 func (apiConfig *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User){
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
+
+func (apiConfig *apiConfig)handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User){
+	posts, err := apiConfig.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit: 10,
+	})
+
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
+}
