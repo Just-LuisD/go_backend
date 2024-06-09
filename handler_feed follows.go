3a415f37d9d861c4c16af916efcb85b978923e51
Just-LuisD <luis.e.duran.engr@gmail.com>
2024-06-08 20:24:19 -0500

    finished crud functions

---
+++ handler_feed follows.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Just-LuisD/rss-aggregator/internal/database"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+)
+
+func (apiConfig *apiConfig)handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User){
+	type parameters struct{
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Error parsiong JSON: %v", err))
+		return
+	}
+
+	feedFollow, err := apiConfig.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID: user.ID,
+		FeedID: params.FeedID,
+	})
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Couldn't create feed follow: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
+}
+
+func (apiConfig *apiConfig)handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user database.User){
+	feedFollows, err := apiConfig.DB.GetFeedFollows(r.Context(), user.ID)
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Couldn't get feed follows: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiConfig *apiConfig)handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User){
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Couldn't parse feed follow id: %v", err))
+	}
+
+	err = apiConfig.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID: feedFollowID,
+		UserID: user.ID,
+	})
+
+	if err != nil{
+		respondWithErr(w, 400, fmt.Sprintf("Couldn't delete feed follow %v", err))
+	}
+
+	respondWithJSON(w, 200, struct{}{})
+}
